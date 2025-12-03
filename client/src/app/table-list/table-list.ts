2f5bd850248afd0e6065c55029aa073bf1b45535
Qui Nguyen <qui.nguyen@cubable.com>
2025-12-03 21:46:30 +0700

    refactor: replace spreadsheet component with table list and detail components for improved structure and maintainability

---
+++ client/src/app/table-list/table-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { Button } from 'primeng/button';
+import { Divider } from 'primeng/divider';
+
+@Component({
+  selector: 'app-table-list',
+  imports: [Button, Divider],
+  templateUrl: './table-list.html',
+})
+export class AppTableList {}
