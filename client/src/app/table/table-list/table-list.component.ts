1d0650185c3be7270de64c6d891ed462933f0578
Qui Nguyen <qui.nguyen@cubable.com>
2025-12-09 22:31:02 +0700

    feat: enhance table list component with search functionality, refresh button, and improved loading state management

--- client/src/app/table/table-list/table-list.component.ts
+++ client/src/app/table/table-list/table-list.component.ts
@@ -1,16 +1,24 @@
+import _ from 'lodash';
 import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { TableService, TableDefinition } from '../table.service';
 
 @Component({
   selector: 'app-table-list',
-  imports: [ButtonModule],
+  imports: [FormsModule, ButtonModule, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './table-list.component.html',
 })
 export class AppTableList {
   protected tables = signal<TableDefinition[]>([]);
+  protected filteredTables = signal<TableDefinition[]>([]);
+  protected isLoading = signal<boolean>(false);
+  protected searchQuery = '';
 
   protected tblService = inject(TableService);
 
@@ -22,10 +30,39 @@ export class AppTableList {
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((tables) => {
         this.tables.set(tables);
+        this.filteredTables.set(tables);
       });
   }
 
-  onTableSelected(table: TableDefinition) {
+  protected onTableSearch(query: string) {
+    const tables = this.tables();
+    if (query.length) {
+      this.filteredTables.set(
+        tables.filter(
+          (t) => t.tableName.search(query) !== -1 || (t.tableComment || '').search(query) !== -1,
+        ),
+      );
+    } else {
+      this.filteredTables.set([...tables]);
+    }
+  }
+
+  protected onTableSelected(table: TableDefinition) {
     this.tblService.selectedTable.set(table);
+  }
+
+  protected refreshTables = _.debounce(() => this.getTables(), 1000, { leading: true });
+
+  private getTables() {
+    this.isLoading.set(true);
+    this.tblService
+      .getTables()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((tables) => {
+        this.tables.set(tables);
+        this.filteredTables.set(tables);
+        this.searchQuery = '';
+        this.isLoading.set(false);
+      });
   }
 }
